338fff0f545a4600595bccffd4de3e64c3562689
= <=>
2020-03-22 18:02:18 +1100

    trying to get cors working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,38 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = () => {
+  
+  const [HW, setHW] = useState(0)
+  useEffect(() => {
+    // get data from GitHub api
+    fetch(`https://9il287rnf8.execute-api.us-east-1.amazonaws.com/mvp/`)
+      .then(response => response.json()) // parse JSON from request
+      .then(resultData => {
+        setHW(resultData)
+      }) // set data for the number of stars
+  }, [])
+
+  return (
   <Layout>
     <SEO title="Home" />
     <Link to="/profile/">Go to profile</Link>
     <h1>it's a reddit clone</h1>
     <p>there's some content here</p>
     <p>probably limit to 20 articles on home page</p>
+    <p>The result is: {HW}</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     
   </Layout>
-)
+  )
+
+}
 
 export default IndexPage
