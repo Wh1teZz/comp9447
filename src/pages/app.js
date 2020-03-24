238cc12ccbe3a537a82e9f43bebd08878b548817
= <=>
2020-03-25 10:11:24 +1100

    cleaned up app

--- src/pages/app.js
+++ src/pages/app.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Router } from "@reach/router"
 import Layout from "../components/layout"
 import Profile from "../components/profile"
-import Login from "../components/login"
+import Login from "./login"
 
 const App = () => (
   <Layout>
