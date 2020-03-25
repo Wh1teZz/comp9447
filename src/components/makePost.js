257912ae63f38aacee45bfdc38f2e01bbe227ed4
= <=>
2020-03-26 10:44:45 +1100

    makepost is done

---
+++ src/components/makePost.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react"
+import axios from 'axios';
+import { Link } from "gatsby"
+
+
+function MakePost (channel_id = '') {
+
+  const [values, setValues] = useState({
+    currentText:'',
+    currentTitle:''
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const token = window.sessionStorage.getItem('userToken');
+
+  function submitPost () {
+    const message = values.currentText.trim();
+    const title = values.currentTitle.trim();
+
+    // Quick Validation
+    if (!message || !title) return;
+
+      axios.post(`API`, {
+        token,
+        channel_id,
+        message,
+        title,
+      })
+        .then(({ data }) => {
+          console.log(data);
+        })
+        .catch((err) => { });
+    };
+
+  return (
+  <body>
+    <h1>
+        Make a post
+    </h1>
+    <form noValidate onSubmit={submitPost}>
+      <input type="text"
+        variant="outlined"
+        margin="normal"
+        required
+        type="text"
+        value={values.currentTitle}
+        onChange={handleChange('currentTitle')}
+      />
+      <br />
+      <input type="text"
+        variant="outlined"
+        margin="normal"
+        required
+        value={values.currentText}
+        onChange={handleChange('currentText')}
+      />
+      <br />
+      <button type="submit" fullWidth variant="contained" color="primary">
+        make post
+      </button>
+      <br />
+      <Link to="/">Home</Link>
+    </form>
+  </body>
+)
+}
+
+export default MakePost;
---
+++ src/pages/makePost.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react"
+import Layout from "../components/layout"
+import MakePost from "../components/makePost"
+
+function MakePostPage () {
+  
+  return(
+    <Layout>
+      <MakePost/>
+    </Layout>
+  )
+}
+
+export default MakePostPage;
